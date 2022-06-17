ff566c005ea632a8efaabd9013f15d84c0fc2c1f
Andrzej Karcinski <andrzej.karcinski@gmail.com>
2022-06-17 10:32:55 +0200

    ended up at laser

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform target;
+    private int wavepointindex = 0;
+
+    private Enemy enemy;
+
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+        target = WayPoints.points[0];
+    }
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.3f)
+        {
+            GetNextWayPoint();
+        }
+        enemy.speed = enemy.startSpeed;
+    }
+    void GetNextWayPoint()
+    {
+        if (wavepointindex >= WayPoints.points.Length)
+        {
+            EndPath();
+            return;
+        }
+        wavepointindex++;
+        target = WayPoints.points[wavepointindex];
+    }
+    void EndPath()
+    {
+        PlayerStats.Lives--;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public TurretBlueprint standardTurret;
+    public TurretBlueprint rocketTurret;
+    public TurretBlueprint laserBeamer;
+
+    BuildManager buildManager;
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+    public void SelectStandardTurret()
+    {
+        Debug.Log("Standard Turret Selected");
+        buildManager.SelectTurretToBuild(standardTurret);
+    }
+    public void SelectRocketTurret()
+    {
+        Debug.LogWarning("Rocket Turret Selected");
+        buildManager.SelectTurretToBuild(rocketTurret);
+    }
+    public void SelectLaserBeamer()
+    {
+        Debug.LogWarning("Laser Beameer Selected");
+        buildManager.SelectTurretToBuild(laserBeamer);
+    }
+}
